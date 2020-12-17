7223b9b04c24c4a20091c34fca2a1cb9d8fa151a
Visvaldas <visva.rapalis@gmail.com>
2020-12-17 16:38:50 +0200

    Add smoke animation

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -19,7 +19,25 @@ export default class HelloWorldScene extends Phaser.Scene {
   create() {
     const { width, height } = this.scale;
 
+    const particles = this.add.particles('smoke')
     this.ship = this.add.image(width * 0.5, height * 0.5, 'ship');
+
+    particles.createEmitter({
+      quantity: 10,
+      speedY: { min: 20, max: 50 },
+      speedX: { min: -10, max: 10 },
+      accelerationY: 1000,
+      lifespan: { min: 100, max: 300 },
+      alpha: { start: 0.5, end: 0, ease: 'Sine.easeIn' },
+      scale: { start: 0.065, end: 0.002 },
+      rotate: { min: -180, max: 180 },
+      angle: { min: 30, max: 110 },
+      blendMode: 'ADD',
+      frequency: 15,
+      follow: this.ship,
+      followOffset: { y: this.ship.height * 0.5},
+      tint: 0xed7e77
+    })
   }
 
   update() {

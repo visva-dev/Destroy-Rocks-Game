da6dcd5b818cad937ccc8cf0be759e759d613ba2
Visvaldas <visva.rapalis@gmail.com>
2020-12-17 15:14:38 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = {
+	root: true,
+	parserOptions: { 
+		ecmaVersion: 6,
+		sourceType: 'module'
+	},
+	env: {
+		es6: true,
+		browser: true
+	},
+    extends: [
+		'eslint:recommended'
+	],
+	parser: "babel-eslint",
+	rules: {}
+}

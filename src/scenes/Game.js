7d8bfce64fa13ef98aa7361e36997904059afdcd
Visvaldas <visva.rapalis@gmail.com>
2020-12-23 11:18:08 +0200

    Add player

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,14 +5,24 @@ class Game extends Phaser.Scene {
     super({ key: 'GameScene' });
   }
 
-  init(data) {}
-
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    // this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
   }
 
   create(data) {
-    this.add.image(400, 300, 'logo');
+    this.anims.create({
+      key: 'hero-running',
+      frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+      frameRate: 10,
+      repeat: -1,
+    });
+    // this.add.image(400, 300, 'logo');
+    this.player = this.add.sprite(400, 300, 'hero-run-sheet');
+    this.player.anims.play('hero-running');
   }
 
   update(time, delta) {}

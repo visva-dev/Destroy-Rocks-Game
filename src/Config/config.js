5f4796763a2793a4ee5c9b353bf7c831dd321756
Visvaldas <visva.rapalis@gmail.com>
2021-01-05 12:56:31 +0200

    Install eslint and babel

--- src/Config/config.js
+++ src/Config/config.js
@@ -4,5 +4,13 @@ export default {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 800,
-  height: 600
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
+  pixelArt: true,
+  roundPixels: true,
 };

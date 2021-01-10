41b5ac0661388b881abc9f5c0d8dcedd4b5a3a16
Visvaldas <visva.rapalis@gmail.com>
2021-01-10 11:03:29 +0200

    Add LeaderBoard & background music

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,7 +1,10 @@
 import * as Phaser from 'phaser';
-import SceneMainMenu from '../Scenes/SceneMainMenu';
+import InputTextPlugin from 'phaser3-rex-plugins/plugins/inputtext-plugin';
 import SceneMain from '../Scenes/SceneMain';
+import SceneMainMenu from '../Scenes/SceneMainMenu';
 import SceneGameOver from '../Scenes/SceneGameOver';
+
+const seedRand = Date.now();
 
 const config = {
   type: Phaser.WEBGL,
@@ -17,12 +20,23 @@ const config = {
   scene: [SceneMainMenu, SceneMain, SceneGameOver],
   pixelArt: true,
   roundPixels: true,
+  dom: {
+    createContainer: true,
+  },
+  seed: [seedRand],
+  plugins: {
+    global: [{
+      key: 'rexInputTextPlugin',
+      plugin: InputTextPlugin,
+      start: true,
+    }],
+  },
 };
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
-    this.scene.start('Entity');
+    this.scene.start('SceneMainMenu');
   }
 }
 
@@ -31,5 +45,7 @@ window.onload = () => {
 };
 
 window.global = {
+  signature: 'Made by Visvaldas Rapalis',
   score: 0,
+  emitter: null,
 };
--- src/Scenes/SceneMainMenu.js
+++ src/Scenes/SceneMainMenu.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-plusplus */
 import * as Phaser from 'phaser';
+import STYLE from '../styles/style';
 import ScrollingBackground from '../Objects/ScrollingBackground';
 
 export default class SceneMainMenu extends Phaser.Scene {
@@ -8,6 +9,40 @@ export default class SceneMainMenu extends Phaser.Scene {
   }
 
   preload() {
+    this.load.plugin('rexinputtextplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexinputtextplugin.min.js', true);
+
+    const assetText = this.make.text({
+      x: window.global.width / 2,
+      y: (window.global.height / 2 - 70),
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: STYLE.colors.white,
+      },
+    })
+      .setOrigin(0.5);
+    const loadingText = this.make.text({
+      x: assetText.x,
+      y: assetText.y + 32,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: STYLE.colors.white,
+      },
+    })
+      .setOrigin(0.5);
+    this.load.on('progress', value => {
+      // eslint-disable-next-line radix
+      loadingText.setText(`${parseInt(value * 100)} %`);
+    });
+    this.load.on('fileprogress', file => {
+      assetText.setText(`Loading asset: ${file.key}`);
+    });
+    this.load.on('complete', () => {
+      assetText.destroy();
+      loadingText.destroy();
+    });
+
     this.load.image('sprBg0', 'assets/ui/sprBg0.png');
     this.load.image('sprBg1', 'assets/ui/sprBg1.png');
     this.load.image('sprBtnPlay', 'assets/ui/sprBtnPlay.png');
@@ -18,9 +53,20 @@ export default class SceneMainMenu extends Phaser.Scene {
     this.load.image('sprBtnRestartDown', 'assets/ui/sprBtnRestartDown.png');
     this.load.audio('sndBtnOver', 'assets/sounds/sndBtnOver.wav');
     this.load.audio('sndBtnDown', 'assets/sounds/sndBtnDown.wav');
+    this.load.audio('bgm', ['assets/audio/bgm_bit.mp3', 'assets/sounds/bgm_bit.ogg']);
   }
 
   create() {
+    this.add.text(2, this.game.config.height - 2,
+      `Play Control\nMove: [A (Left), D (Right), W (Up), S (Down)]\nShoot: [Space]\n${window.global.signature}`)
+      .setOrigin(0, 1);
+
+    if (window.global.bgmInstance === undefined) {
+      this.bgm = this.sound.add('bgm', { loop: true, volume: 0.5 });
+      this.bgm.play();
+      window.global.bgmInstance = this.bgm;
+    }
+
     this.sfx = {
       btnOver: this.sound.add('sndBtnOver'),
       btnDown: this.sound.add('sndBtnDown'),
@@ -78,6 +124,33 @@ export default class SceneMainMenu extends Phaser.Scene {
       },
     );
     this.title.setOrigin(0.5);
+
+    const printText = this.add.text(240, 240, '', {
+      fontSize: '12px',
+      fixedWidth: 100,
+      fixedHeight: 100,
+    }).setOrigin(0.5);
+    const inputText = this.add.rexInputText(240, 260, 200, 30, {
+      type: 'text',
+      placeholder: 'Enter player name',
+      fontSize: STYLE.fonts.small,
+      color: STYLE.colors.white,
+      borderBottom: `3px solid ${STYLE.colors.gold}`,
+    })
+      .setOrigin(0.5)
+      .on('textchange', () => {
+        printText.text = inputText.text;
+      });
+
+    printText.text = inputText.text;
+
+    this.submitButton = this.add.text(240, 300, 'Submit Name').setInteractive().setOrigin(0.5);
+    this.submitButton.on('pointerdown', () => {
+      if (printText.text.length > 0) {
+        window.global.userName = printText.text;
+        this.submitButton.destroy();
+      }
+    });
 
     this.backgrounds = [];
     for (let i = 0; i < 5; i++) {
